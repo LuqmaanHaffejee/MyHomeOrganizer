5a26b1b2571852f389badc7dd9513f4261ef89d5
Luqmaan Haffejee <luqhaf5@gmail.com>
2024-03-14 10:24:17 +0200

    basic component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './domain/shopping-list/ui/shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from "./domain/shopping-list/ui/shopping-list-edit/shopping-list-edit.component";
+import {RecipeListComponent} from "./domain/recipe-book/ui/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from './domain/recipe-book/ui/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './domain/recipe-book/ui/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from "./shared/ui/header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
