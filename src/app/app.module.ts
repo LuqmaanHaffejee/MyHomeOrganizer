50cb0f785650cabbe8d5704c133c2441c6895045
Luqmaan Haffejee <luqhaf5@gmail.com>
2024-03-14 11:06:42 +0200

    basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './domain/shopping-list/ui/shopping-list/shopping-list.component';
-import {ShoppingListEditComponent} from "./domain/shopping-list/ui/shopping-list-edit/shopping-list-edit.component";
-import {RecipeListComponent} from "./domain/recipe-book/ui/recipe-list/recipe-list.component";
-import { RecipeItemComponent } from './domain/recipe-book/ui/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './domain/recipe-book/ui/recipe-detail/recipe-detail.component';
+import {ShoppingListEditComponent} from "./domain/shopping-list/ui/shopping-list/shopping-list-edit/shopping-list-edit.component";
+import {RecipeListComponent} from "./domain/recipe-book/ui/recipe/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from './domain/recipe-book/ui/recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './domain/recipe-book/ui/recipe/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from "./shared/ui/header/header.component";
+import { RecipeComponent } from './domain/recipe-book/ui/recipe/recipe.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HeaderComponent } from "./shared/ui/header/header.component";
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
