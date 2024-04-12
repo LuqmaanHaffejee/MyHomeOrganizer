4938fa7d26f847ec304050442e60bc85a8b68f58
luqmaan <luqhaf5@gmail.com>
2024-04-12 14:06:55 +0200

    template driven forms for shopping list form

--- src/app/domain/shopping-list/ui/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/domain/shopping-list/ui/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, inject, ViewChild } from "@angular/core";
 import { ShoppingListService } from "../../../services/shopping-list.service";
 import { Ingredient } from "../../../../../shared/model/ingredient";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -9,17 +10,14 @@ import { Ingredient } from "../../../../../shared/model/ingredient";
 })
 export class ShoppingListEditComponent {
 
+  @ViewChild('formRef')
+  formRef: NgForm;
+
   shoppingListService = inject(ShoppingListService);
 
-  @ViewChild('nameInput')
-  nameInput: ElementRef;
-
-  @ViewChild('amountInput')
-  amountInput: ElementRef;
-
   onAddIngredient() {
-    const name: string = this.nameInput.nativeElement.value;
-    const amount: number = this.amountInput.nativeElement.value;
+    const name: string = this.formRef.form.get('name').value;
+    const amount: number = +this.formRef.form.get('amount').value;
     this.shoppingListService.addIngredient(new Ingredient(name, amount));
   }
 }
